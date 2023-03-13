bac2e00fd256f14de17e2e2f4801b09fd59c6081
Serg <serg212007@gmail.com>
2023-03-13 17:05:13 +0200

    clean code

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -9,16 +9,3 @@ const validation = (schema) => {
   };
 };
 module.exports = validation;
-
-// const validation = (schema) => {
-//   return (req, res, next) => {
-//     const { error } = schema.validate(req.body);
-//     if (error) {
-//       error.status = 400;
-//       next(error);
-//       return;
-//     }
-//     next();
-//   };
-// };
-// module.exports = validation;

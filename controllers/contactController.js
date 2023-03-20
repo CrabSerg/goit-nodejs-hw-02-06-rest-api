f824105336bc8f77931f15a0a466abdadbccf063
Serg <serg212007@gmail.com>
2023-03-20 13:39:54 +0200

    get

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,8 +1,24 @@
+const { NotFound } = require("http-errors");
+
+const  Contact  = require("../models/contacts");
+
 const getContact = async (req, res, next) => {
   try {
     const contacts = await Contact.find();
 
     res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const contactByIdGet = async (req, res, next) => {
+  try {
+    const IdContact = await Contact.findById(req.params.contactId);
+    if (!IdContact) {
+      throw new NotFound({ message: "not found" });
+    }
+    res.status(200).json(IdContact);
   } catch (error) {
     next(error);
   }

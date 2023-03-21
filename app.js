e730c459d11f714fa07d8a2b50baa582dd4ea536
Serg <serg212007@gmail.com>
2023-03-21 13:25:45 +0200

    test

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
+
+
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server Error" } = err;
   res.status(status).json({ message });
